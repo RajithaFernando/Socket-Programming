94b6ce8d2b7df275479e27c15f84013f7265bcc4
rajitha <rajithaf@gmail.com>
2018-10-28 16:15:31 +0530

    working server configuration no file handle

--- server.py
+++ server.py
@@ -1,37 +1,62 @@
 from socket import *
 
+
+HOST, PORT = '127.0.0.1', 9000
+
 #https://docs.python.org/2/howto/sockets.html
 
 
 '''
 
-serversocket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind((socket.gethostname(), 80))
-serversocket.listen(5)
+    serversocket = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((socket.gethostname(), 80))
+    serversocket.listen(5)
 
 '''
 
 s = socket(AF_INET,SOCK_STREAM)
 
-s.connect(("www.python.org",80))  #s.connect(addr) makes a connection
-s.bind(("127.0.0.1",9000)) #   only visible within the same machine
+#s.connect(("",9000))  #s.connect(addr) makes a connection
+s.bind((HOST,PORT)) #   only visible within the same machine
+#s.bind(('', 80)) specifies that the socket is reachable by any address the machine happens to have.
+
 s.listen(5)
 
+print ('Serving HTTP on port %s ...' % PORT)
+# while 1: # I think python V-2
 
-# while 1:
+
+''' 
+    # accept connections from outside
+    (clientsocket, address) = serversocket.accept()
+    # now do something with the clientsocket
+    # in this case, we'll pretend this is a threaded server
+    ct = client_thread(clientsocket)
+    ct.run()
+'''
 
 while True:
-    c,a = s.accept()
-    print ("Received connection from", a)
-    c.send("Hello %s\n" % a[0])
+    c,a = s.accept() # c = cloent socket , a = address
+    request = c.recv(1024)
+    print (request)
+
+
+
+    http_response = b"""\
+HTTP/1.1 200 OK
+
+Hello, World!
+"""
+    c.send(http_response)
     c.close()
 
 
 
+'''
 
+    s.send("GET /index.html HTTP/1.0\n\n")   #send request
+    data = s.recv(10000)    # get request
+    s.close()   #shuts down the connection
 
-s.send("GET /index.html HTTP/1.0\n\n")   #send request
-data = s.recv(10000)    # get request
-s.close()   #shuts down the connection
-
+'''
